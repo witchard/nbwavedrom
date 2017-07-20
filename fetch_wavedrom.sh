20273b7dfd92f9fdb4fbc0e68c54e32d1049e467
witchard <witchard@hotmail.co.uk>
2017-07-20 19:45:47 +0100

    Updated to latest wavedrom-cli

--- fetch_wavedrom.sh
+++ fetch_wavedrom.sh
@@ -2,3 +2,4 @@
 
 curl http://wavedrom.com/wavedrom.min.js -o js/wavedrom.min.js
 curl http://wavedrom.com/skins/default.js -o js/wavedromskin.js
+curl https://raw.githubusercontent.com/wavedrom/cli/master/bin/wavedrom-cli.js -o js/wavedrom-cli.js
